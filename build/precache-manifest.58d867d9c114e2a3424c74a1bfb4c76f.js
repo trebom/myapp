a57a6e829feaf6a1f2109ea4ebdcc3513ac30c9a
Your Name Here <shinykim63@gmail.com>
2020-10-18 01:15:23 -0400

    update

---
+++ build/precache-manifest.58d867d9c114e2a3424c74a1bfb4c76f.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68dcf77228d019adf8467efe3946c834",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "1a98a39f8530146b97dc",
+    "url": "/myapp/static/js/main.5bf3c1fa.chunk.js"
+  },
+  {
+    "revision": "78857309b027b3eb01f0",
+    "url": "/myapp/static/js/runtime-main.6e84b50a.js"
+  }
+]);
